864f0f2ac9dfb118eb3776c36a6f1c972f8fb36e
Anna Taranchuk <taranchuk4@yahoo.com>
2024-06-23 19:05:17 +0300

    update App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,50 +1,40 @@
-import { useEffect, lazy } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Route, Routes } from "react-router-dom";
-import Layout from "../Layout/Layout";
-import css from "./App.module.css";
-// import ContactForm from "../ContactForm/ContactForm";
-// import ContactList from "../ContactList/ContactList";
-// import SearchBox from "../SearchBox/SearchBox";
-// import Loader from "../Loader/Loader";
-// import Error from "../Error/Error";
-// import { fetchContacts } from "../../redux/contactsOps";
-// import { selectError, selectLoading } from "../../redux/contactsSlice";
+import { useEffect, lazy, Suspense } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import Layout from '../Layout/Layout';
 
-const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
 const ContactsPage = lazy(() =>
-  import("../../pages/ContactsPage/ContactsPage")
+  import('../../pages/ContactsPage/ContactsPage')
 );
-const LoginPage = lazy(() => import("../../pages/LoginPage/LoginPage"));
+const LoginPage = lazy(() => import('../../pages/LoginPage/LoginPage'));
 const RegisterPage = lazy(() =>
-  import("../../pages/RegisterPage/RegisterPage")
+  import('../../pages/RegisterPage/RegisterPage')
 );
 
 export default function App() {
-  // const dispatch = useDispatch();
-  // const isLoading = useSelector(selectLoading);
-  // const isError = useSelector(selectError);
-
-  // useEffect(() => {
-  //   dispatch(fetchContacts());
-  // }, [dispatch]);
-
   return (
-    // <div className={css.container}>
-    //   <h1 className={css.title}>Phonebook</h1>
-    //   <ContactForm />
-    //   <SearchBox />
-    //   {isLoading && <Loader />}
-    //   {isError && <Error />}
-    //   <ContactList />
-    // </div>
     <Layout>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/contacts" element={<ContactsPage />} />
-      </Routes>
+      <Suspense fallback={null}>
+        <Routes>
+          <Route
+            path='/'
+            element={<HomePage />}
+          />
+          <Route
+            path='/register'
+            element={<RegisterPage />}
+          />
+          <Route
+            path='/login'
+            element={<LoginPage />}
+          />
+          <Route
+            path='/contacts'
+            element={<ContactsPage />}
+          />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
