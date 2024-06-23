54d141d48a7f911cfb2fe59b268e2ba373558b4c
Anna Taranchuk <taranchuk4@yahoo.com>
2024-06-23 19:07:07 +0300

    update Layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import "modern-normalize";
-import App from "./components/App/App";
-import { store } from "./redux/store";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import 'modern-normalize';
+import App from './components/App/App';
+import { store } from './redux/store';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
+    <Provider store={store}>
+      <BrowserRouter>
         <App />
-      </Provider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
